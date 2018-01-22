b295f9eae9cff2574ef8ae8864b69e2edd16b7a5
nkiru onwuneme <generalhrh@gmail.com>
2018-01-22 11:06:07 -0800

    can add a new sensor

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -5,11 +5,17 @@ class SensorsController < ApplicationController
   end
 
   def new
-
+    @sensor = Sensor.new
   end
 
   def create
-
+    sensor = Sensor.new(sensor_params)
+    if sensor.save
+    render json: { id: sensor.id }, status: :ok
+    else
+    render json: { errors: sensor.errors.messages },
+      status: :bad_request
+    end
   end
 
   def show
@@ -26,6 +32,13 @@ class SensorsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  # Never trust parameters from the scary internet, only allow what is required.
+  def sensor_params
+    params.require(:sensor).permit(:name, :description, :model)
   end
 
 
