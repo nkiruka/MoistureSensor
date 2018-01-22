14cf23e71a042164d5cd2a844b2c58831fb6567d
nkiru onwuneme <generalhrh@gmail.com>
2018-01-22 13:44:55 -0800

    can delete a reading

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -48,7 +48,11 @@ class ReadingsController < ApplicationController
   end
 
   def destroy
-
+    reading.destroy
+    respond_to do |format|
+      format.html { redirect_to measures_url, notice: 'Moisture reading was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
