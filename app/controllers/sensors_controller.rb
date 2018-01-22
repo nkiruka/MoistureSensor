c01090f96a539d4bd2bde90c424feb46af25f412
nkiru onwuneme <generalhrh@gmail.com>
2018-01-22 10:31:23 -0800

    created Sensor controller and moel

---
+++ app/controllers/sensors_controller.rb
@@ -0,0 +1,33 @@
+class SensorsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+
+end
