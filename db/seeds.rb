ffafb13fb45d5c84f2924f6715670d834598606d
nkiru onwuneme <generalhrh@gmail.com>
2018-01-22 10:51:47 -0800

    seeded sensor and reading data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Sensor.create!([
+  {name: "Sensor 1", description: "moisture ", model: "Y-161"},
+  {name: "Sensor 2", description: "moisture", model: "Y-161"},
+  {name: "Sensor 3", description: "moisture", model: "Y-161"},
+  {name: "Sensor 4", description: "moisture", model: "Y-161"},
+  {name: "Sensor 5", description: "moisture", model: "Y-161"},
+  {name: "Sensor 6", description: "moisture", model: "Y-161"},
+  {name: "Sensor 7", description: "moisture", model: "Y-161"},
+  {name: "Sensor 8", description: "moisture", model: "Y-161"},
+  {name: "Sensor 9", description: "moisture", model: "Y-161"},
+  {name: "Sensor 10", description: "moisture", model: "Y-161"},
+  {name: "Sensor 11", description: "moisture", model: "Y-161"},
+  {name: "Sensor 12", description: "moisture", model: "Y-161"}
+])
+
+Reading.create!([
+  {sensor_id: 1, value: 50, created_at: Time.now},
+  {sensor_id: 1,value: 55, created_at: Time.now+2},
+  {sensor_id: 1,value: 60, created_at: Time.now+3},
+  {sensor_id: 1,value: 65, created_at: Time.now+4},
+  {sensor_id: 1,value: 70, created_at: Time.now+5},
+  {sensor_id: 1,value: 75, created_at: Time.now+6},
+  {sensor_id: 1,value: 80, created_at: Time.now+7},
+  {sensor_id: 1,value: 85, created_at: Time.now+8},
+  {sensor_id: 1,value: 90, created_at: Time.now+9},
+  {sensor_id: 1,value: 95, created_at: Time.now+10},
+  {sensor_id: 2, value: 30, created_at: Time.now},
+  {sensor_id: 2, value: 29, created_at: Time.now+2},
+  {sensor_id: 2, value: 29, created_at: Time.now+3},
+  {sensor_id: 2, value: 28, created_at: Time.now+4},
+  {sensor_id: 2, value: 20, created_at: Time.now+5},
+  {sensor_id: 2, value: 30, created_at: Time.now+6},
+  {sensor_id: 2, value: 31, created_at: Time.now+7},
+  {sensor_id: 2, value: 31, created_at: Time.now+8},
+  {sensor_id: 2, value: 32, created_at: Time.now+9}
+])
