6f11a670a45961c1f06bbd21096f13459bfcd6cd
nkiru onwuneme <generalhrh@gmail.com>
2018-01-22 13:25:59 -0800

    can create a new reading

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -10,14 +10,25 @@ class ReadingsController < ApplicationController
 
 
   def new
-
+    @reading = Reading.new
   end
 
   def edit
   end
 
   def create
+    @reading = Reading.new(reading_params)
 
+    # respond_to do |format|
+      if @reading.save
+        # format.html { redirect_to @reading, notice: 'Reading was successfully created.' }
+        # format.json { render :show, status: :created, location: @reading }
+        render json: { id: @reading.id }, status: :ok
+      else
+        # format.html { render :new }
+        format.json { render json: @reading.errors, status: :unprocessable_entity }
+      end
+    # end
   end
 
 
@@ -29,5 +40,10 @@ class ReadingsController < ApplicationController
 
   end
 
+  private
+    # Never trust parameters from the scary internet, only allow permitted values.
+    def reading_params
+      params.require(:reading).permit(:sensor_id, :value)
+    end
 
 end
