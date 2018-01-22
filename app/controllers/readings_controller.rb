eb71975fbe63d1d9404c41e849c93e0fd0dbb3b5
nkiru onwuneme <generalhrh@gmail.com>
2018-01-22 10:38:34 -0800

    Create Reading controller and model

---
+++ app/controllers/readings_controller.rb
@@ -0,0 +1,35 @@
+class ReadingsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+
+  end
+
+  def show
+  end
+
+
+  def new
+
+  end
+
+  def edit
+  end
+
+  def create
+
+  end
+
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180122182725) do
+ActiveRecord::Schema.define(version: 20180122183400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "readings", force: :cascade do |t|
+    t.integer "sensor_id"
+    t.integer "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sensors", force: :cascade do |t|
     t.string "name"
