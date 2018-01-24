7de05251ab9b8972a9bdfcdb0a9f13128dc44cb6
nkiru onwuneme <generalhrh@gmail.com>
2018-01-24 11:44:02 -0800

    added controller action

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,9 +1,9 @@
 class Reading < ApplicationRecord
-  belongs_to :sensor
-  belongs_to :plant
+  # belongs_to :sensor
+  # belongs_to :plant
 
-  validates_presence_of :sensor_id
-  validates_numericality_of :value  #numericality: { only_integer: true}
+  # validates_presence_of :sensor_id
+  # validates_numericality_of :value  #numericality: { only_integer: true}
   # numericality: { only_integer: true, greater_than: 0, less_than: 6 }
   # greater than or equal to zero, but can't be more than 100
 end
