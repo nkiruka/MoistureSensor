e82730c72c6b704f836ce03c53ff96e05c073681
nkiru onwuneme <generalhrh@gmail.com>
2018-01-24 09:24:39 -0800

    added plant_id to readings table

---
+++ db/migrate/20180124172207_add_plantid_to_readings.rb
@@ -0,0 +1,5 @@
+class AddPlantidToReadings < ActiveRecord::Migration[5.1]
+  def change
+    add_column :readings, :plant_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124114809) do
+ActiveRecord::Schema.define(version: 20180124172207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180124114809) do
     t.integer "value"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "plant_id"
   end
 
   create_table "sensors", force: :cascade do |t|
