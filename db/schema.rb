b8f2fa102f07f6d9a09a591b66029211f585fc43
nkiru onwuneme <generalhrh@gmail.com>
2018-01-24 09:31:18 -0800

    added plantid to sensors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180124172207) do
+ActiveRecord::Schema.define(version: 20180124172551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20180124172207) do
     t.string "model"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "plant_id"
   end
 
 end
