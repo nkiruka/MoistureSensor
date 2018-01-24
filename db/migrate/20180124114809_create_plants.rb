3858199356d89e847fcd1f38d270d6780ce52174
nkiru onwuneme <generalhrh@gmail.com>
2018-01-24 03:55:42 -0800

    create plants table

--- db/migrate/20180124114809_create_plants.rb
+++ db/migrate/20180124114809_create_plants.rb
@@ -1,6 +1,8 @@
 class CreatePlants < ActiveRecord::Migration[5.1]
   def change
     create_table :plants do |t|
+      t.string :name
+      t.integer :threshold
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180122183400) do
+ActiveRecord::Schema.define(version: 20180124114809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "plants", force: :cascade do |t|
+    t.string "name"
+    t.integer "threshold"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "readings", force: :cascade do |t|
     t.integer "sensor_id"
